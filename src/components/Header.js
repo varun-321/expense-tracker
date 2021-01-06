6516ba8226405af5ad1119c51e327175eb646c15
varun <2017283@iiitdmj.ac.in>
2021-01-06 18:32:20 +0530

    First Commit

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+  const Header = () => {
+    return (
+        <div>
+            
+                 
+                     <header>
+                 Expense Tracker 
+
+                  </header>
+    
+
+        </div>
+    )
+}
+   export default Header
